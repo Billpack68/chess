e1f00398baf4e8343f93e6df01c9d27f59e9b098
Bill Killpack <trombonebill6912@gmail.com>
2025-11-03 16:13:42 -0700

    Negative test for AddUser

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -3,9 +3,12 @@ import model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mindrot.jbcrypt.BCrypt;
+import service.MissingDataException;
 
 import java.sql.SQLException;
 import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DataAccessTests {
     private UserDAO userDAO;
@@ -28,5 +31,15 @@ public class DataAccessTests {
         assert(Objects.equals(newUser.username(), user.username()));
         assert(BCrypt.checkpw(newUser.password(), user.password()));
         assert(Objects.equals(newUser.email(), user.email()));
+    }
+
+    @Test
+    void TestRegisterDuplicateUsername() throws SQLException, DataAccessException {
+        UserData newUser = new UserData("username", "password", "email");
+        userDAO.addUserData(newUser);
+        UserData sameUsername = new UserData("username", "password2", "email2");
+        assertThrows(DataAccessException.class, () -> {
+            userDAO.addUserData(sameUsername);
+        });
     }
 }
