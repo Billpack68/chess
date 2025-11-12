120ad0c95e6aeecf32dcbab36eb8a2e9edd57d2f
Bill Killpack <trombonebill6912@gmail.com>
2025-11-12 10:22:33 -0700

    Impl listGames

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,17 +1,17 @@
 package ui;
 
+import chess.ChessPiece;
 import model.*;
 import server.Server;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class ChessClient {
     private String clientName;
     private String authToken;
     private final ServerFacade server;
     private State state = State.SIGNEDOUT;
+    private Map<Integer, Integer> IDConverter = new HashMap<>();
 
     public ChessClient(String serverUrl) {
         server = new ServerFacade(serverUrl);
@@ -46,7 +46,7 @@ public class ChessClient {
             case "register" -> register(params);
             case "login" -> login(params);
             case "logout" -> logout(params);
-//            case "list" -> listGames(params);
+            case "list" -> listGames(params);
             case "create" -> createGame(params);
 //            case "join" -> joinGame(params);
 //            case "observe" -> observe(params);
@@ -57,7 +57,11 @@ public class ChessClient {
     }
 
     private void printPrompt() {
-        System.out.print("\n>>> ");
+        if (clientName != null) {
+            System.out.print("\n" + clientName + " >>> ");
+        } else {
+            System.out.print("\n>>> ");
+        }
     }
 
     private String help() {
@@ -139,8 +143,9 @@ public class ChessClient {
                 return "Oops! Looks like something went wrong with logging in. Can you try again?";
             }
         }
+        clientName = params[0];
         state = State.SIGNEDIN;
-        return "Logged in as " + params[0] + ". Type `help` to see a list of commands.";
+        return "Logged in as " + clientName + ". Type `help` to see a list of commands.";
     }
 
     private String logout(String... params) {
@@ -162,6 +167,7 @@ public class ChessClient {
                 return "Oops! Looks like something went wrong with logging out. Can you try again?";
             }
         }
+        clientName = null;
         state = State.SIGNEDOUT;
         return "Successfully logged out!";
     }
@@ -188,6 +194,55 @@ public class ChessClient {
             }
         }
         return "Successfully created game! Use `list` to see the details of that game.";
+    }
+
+    private String listGames(String... params) {
+        try {
+            assertSignedIn();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+        if (params.length != 0) {
+            return "Too many arguments. Please just type `list`";
+        }
+
+        StringBuilder outString = new StringBuilder("Games currently being played:");
+        try {
+            ListGamesRequest request = new ListGamesRequest(authToken);
+            ListGamesResult result = server.listGames(request);
+            Collection<GameData> games = result.games();
+
+            for (GameData data : games) {
+                if (!IDConverter.containsKey(data.gameID())) {
+                    IDConverter.put(data.gameID(), IDConverter.size()+1);
+                }
+                StringBuilder gameString = new StringBuilder("\n");
+                gameString.append(IDConverter.get(data.gameID())).append(" |log ").append(data.gameName());
+                gameString.append(" | White: ");
+                if (data.whiteUsername() == null) {
+                    gameString.append("available");
+                } else {
+                    gameString.append(data.whiteUsername());
+                }
+                gameString.append(" | Black: ");
+                if (data.blackUsername() == null) {
+                    gameString.append("available");
+                } else {
+                    gameString.append(data.blackUsername());
+                }
+                outString.append(gameString);
+            }
+            if (games.isEmpty()) {
+                outString.append("\nLooks like there's nothing here yet!");
+            }
+        } catch (ServerFacadeException e) {
+            if (e.getId() == 401) {
+                return "Looks like you aren't authorized to do that. Please try again.";
+            } else {
+                return "Oops! Looks like something went wrong with logging out. Can you try again?";
+            }
+        }
+        return outString.toString();
     }
 
     private void assertSignedIn() throws Exception {
